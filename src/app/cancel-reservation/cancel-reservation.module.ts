bb306c74421da0e91f0eba025ac7bf2d4b3bc36a
SebastianDochia <Sebastian.dochia@gmail.com>
2023-06-17 13:45:16 +0300

    feat: cancel page

--- src/app/cancel-reservation/cancel-reservation.module.ts
+++ src/app/cancel-reservation/cancel-reservation.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import {
   CancelReservationRoutingModule,
@@ -16,6 +18,8 @@ import {
   imports: [
     CommonModule,
     CancelReservationRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   exports: [],
